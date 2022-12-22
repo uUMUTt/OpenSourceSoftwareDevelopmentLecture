51ba1be1c8111c308d8e25b56960a381e99ebd9c
RamazanKpk <100092225+RamazanKpk@users.noreply.github.com>
2022-12-22 18:27:28 +0300

    Added main function for binary search
    I noticed that there is no main function in your code and I added a main function. After reviewing the code, you can accept the pull request.

--- pullrequestgörev/BinarySearch.java
+++ pullrequestgörev/BinarySearch.java
@@ -15,6 +15,20 @@ public class BinarySearch {
         }
         return -1;
     }
+    public static void main(String args[]){
+        BinarySearch bs = new BinarySearch();
+        int arr[] = {4,8,6,9,3};
+        int n = arr.length;
+        int x = 6;
+        int result = bs.binarySearch(arr,x);
+        if (result == -1){
+            System.out.println("Element not present");
+        }
+        else{
+            System.out.println("Element found at"+ "index"+ result);
+        }
+
+    }
 
     
 }
